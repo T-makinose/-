e47e25abda37a3593be4bd9d9c162190bc6714cb
ryotaro2828 <75828657+ryotaro2828@users.noreply.github.com>
2021-07-01 12:46:29 +0900

    Create showreviewctrl.js

---
+++ サンプルコード-20210611/rich/showreviewctrl.js
@@ -0,0 +1,33 @@
+function Showreviewctrl (){
+}
+
+Showreviewctrl.prototype.show = function(id) {
+  this.id = id;
+}
+
+Showreviewctrl.prototype.edit = function(id) {
+  this.id = id;
+}
+
+Showreviewctrl.prototype.close = function(id, txt) {
+  window.location.href = 'subjectlist.html';
+}
+
+$(function(){
+  var shre = new Showreviewctrl();
+  var TransitionSource = document.referrer;
+
+  if (TransitionSource = 'subjectlist.html') {
+    shre.show();
+  } else {
+    shre.edit();
+  }
+
+	document.getElementById("close").onclick = function() {
+    shre.close();
+  };
+
+  document.getElementById("edit").onclick = function() {
+    shre.edit();
+    };
+});
