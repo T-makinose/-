60bef6ac5ba9cdb2ca70d82b353b675449b6b992
tkdshi <62202033+tkdshi@users.noreply.github.com>
2021-07-02 17:16:49 +0900

    Update student.php

--- サンプルコード-20210611/rich/student.php
+++ サンプルコード-20210611/rich/student.php
@@ -2,18 +2,32 @@
 header('content-type: application/json; charset=utf-8');
 
 class Student {
-  public $subjects = array("科目1", "科目2", "科目3");
-  public function vote() {
-    return array('vote' => "dummy_string");
+
+  private $studentNo = 0;
+  
+  public function subjects() {
+    return [1,2,3,4,5];
   }
+
+  public function getReviewText($id){
+    return "TestStr";
+  }
+
+  public function setReviewText($id,$text){
+    return;
+  }
+
+  
 }
 
 $st = new Student();
 
 if($_GET['method'] === "subjects"){
-  echo json_encode($st->subjects);
-}else if($_GET['method'] === "vote"){
-  echo json_encode($st->vote());
+  echo json_encode($st->subjects());
+}else if($_GET['method'] === "getReviewText"){
+  echo json_encode($st->getReviewText($id));
+}else if($_GET['method'] === "setReviewText"){
+  echo json_encode($st->setReviewText($id,$text));
 }else{
   echo json_encode(array('error'=>"unknown_method"));
 }
