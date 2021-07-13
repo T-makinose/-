f1a2269de08419753366958b1b58d6c77885e5bd
o-atht <85736584+o-atht@users.noreply.github.com>
2021-07-13 17:40:31 +0900

    Update student.php

--- サンプルコード-20210611/rich/student.php
+++ サンプルコード-20210611/rich/student.php
@@ -2,36 +2,18 @@
 header('content-type: application/json; charset=utf-8');
 
 class Student {
-
-  private $studentNo = 0;
-  private $review = ['科目0のレビューです。','科目1のレビューです。','科目2のレビューです。','科目3のレビューです。','科目4のレビューです。'];
-
-
-  public function subjects() {
-    return [0,1,2,3,4];
+  public $subjects = array("科目1", "科目2", "科目3");
+  public function vote() {
+    return array('vote' => "dummy_string");
   }
-
-  public function getReviewText($id){
-
-    return $this->review[$id];
-  }
-
-  public function setReviewText($id,$text){
-    $this->review[$id] = $text;
-    return;
-  }
-
-  
 }
 
 $st = new Student();
 
 if($_GET['method'] === "subjects"){
-  echo json_encode($st->subjects());
-}else if($_GET['method'] === "getReviewText"){
-  echo json_encode($st->getReviewText($_GET['id']));
-}else if($_GET['method'] === "setReviewText"){
-  echo json_encode($st->setReviewText($_GET['id'],$_GET['text']));
+  echo json_encode($st->subjects);
+}else if($_GET['method'] === "vote"){
+  echo json_encode($st->vote());
 }else{
   echo json_encode(array('error'=>"unknown_method"));
 }
